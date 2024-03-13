85cb246d96439bc283f30067590f6a1f080bcf57
centpays-tech <centpaysaws@gmail.com>
2024-03-13 11:18:10 +0530

    v2

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
+require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const bodyParser = require("body-parser");
 const ClientRoutes = require("./src/routes/clientroute");
 const UserRoutes = require("./src/routes/userroute");
+const SessionRoutes = require("./src/routes/sessionsroute");
+const AcquirerRoutes = require("./src/routes/acquirerroute");
 
 require("./src/config/database");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
-
+app.use(cors());
+// app.use("", (req, res) => {
+//   res.send("Welcome to Centpays");
+// });
 app.use("/", ClientRoutes);
 app.use("/", UserRoutes);
+app.use("/", SessionRoutes);
+app.use("/", AcquirerRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
