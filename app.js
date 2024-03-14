7f35fac1c1c3b214326805b98187858a3a32df7c
centpays-tech <centpaysaws@gmail.com>
2024-03-14 13:55:07 +0530

    first commit

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const ClientRoutes = require("./src/routes/clientroute");
 const UserRoutes = require("./src/routes/userroute");
 const SessionRoutes = require("./src/routes/sessionsroute");
 const AcquirerRoutes = require("./src/routes/acquirerroute");
+const TransactiontableRoutes = require("./src/routes/transactiontableroute");
+const RatetableRoutes = require("./src/routes/ratetableroute");
+const SettlementtableRoutes = require("./src/routes/settlementroute");
 
 require("./src/config/database");
 
@@ -21,6 +24,9 @@ app.use("/", ClientRoutes);
 app.use("/", UserRoutes);
 app.use("/", SessionRoutes);
 app.use("/", AcquirerRoutes);
+app.use("/", TransactiontableRoutes);
+app.use("/", RatetableRoutes);
+app.use("/", SettlementtableRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
